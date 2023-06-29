fc4697fe33b8c9bebf08a171058c8023a879ef27
yusufyusie <yesuffenta023@gmail.com>
2023-06-29 22:37:43 +0300

    Update the whole files to suppurt the page functional

---
+++ assets/scripts/menu.js
@@ -0,0 +1,26 @@
+function mobileMenu() {
+    const menuIcon = document.querySelector('#hamburger-img');
+    const mainNav = document.querySelector('.nav-bar');
+    const exitIcon = document.querySelector('.exit');
+    const navLinks = document.querySelectorAll('.nav-bar-ul li');
+    const body = document.querySelector('body');
+  
+    menuIcon.addEventListener('click', () => {
+      mainNav.classList.add('nav-bar-active');
+      body.classList.add('overflow-hidden');
+    });
+    exitIcon.addEventListener('click', () => {
+      mainNav.classList.remove('nav-bar-active');
+      body.classList.remove('overflow-hidden');
+    });
+    navLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        mainNav.classList.remove('nav-bar-active');
+        body.classList.remove('overflow-hidden');
+      });
+    });
+  }
+  
+  mobileMenu();
+
+  
