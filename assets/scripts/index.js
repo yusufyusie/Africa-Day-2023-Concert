71d2184f9b41b852a5d419811ede4851555e0d6e
yusufyusie <yesuffenta023@gmail.com>
2023-06-29 17:28:17 +0300

    Update the whole styles and images to support the desktop version

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,44 +1,24 @@
-'use script';
+function mobileMenu() {
+  const menuIcon = document.querySelector('#hamburger-img');
+  const mainNav = document.querySelector('.nav-bar');
+  const exitIcon = document.querySelector('.exit');
+  const navLinks = document.querySelectorAll('.nav-bar-ul li');
+  const body = document.querySelector('body');
 
-const homeBtn = document.getElementById('home-btn');
-const closeBtn = document.getElementById('closeid');
-const navBar = document.getElementById('nav-bar-ul');
-const main = document.getElementById('main');
-const intro = document.getElementById('intro');
-const navBarBlock = document.getElementById('nav-bar');
-const navBarList = document.querySelectorAll('.nav-bar-list');
-const footer = document.getElementById('footer');
-
-/* OPEN MOBILE MENU */
-homeBtn.addEventListener('click', () => {
-  navBar.style.display = 'flex';
-  homeBtn.style.display = 'none';
-  closeBtn.style.display = 'block';
-  main.style.display = 'none';
-  intro.style.display = 'none';
-  footer.style.display = 'none';
-  navBarBlock.style.height = '100vh';
-});
-
-/* CLOSE MOBILE MENU USING MEDIA QUERIES */
-function closeMenu() {
-  navBar.style.display = 'none';
-  homeBtn.style.display = 'block';
-  closeBtn.style.display = 'none';
-  main.style.display = 'block';
-  intro.style.display = 'block';
-  footer.style.display = 'block';
-  navBarBlock.style.height = 'auto';
-}
-
-const mediaQuery = window.matchMedia('(min-width: 768px)');
-function handleTabletChange(e) {
-  if (!e.matches) {
-    closeBtn.addEventListener('click', closeMenu);
-    navBarList.forEach((e) => {
-      e.addEventListener('click', closeMenu);
+  menuIcon.addEventListener('click', () => {
+    mainNav.classList.add('nav-bar-active');
+    body.classList.add('overflow-hidden');
+  });
+  exitIcon.addEventListener('click', () => {
+    mainNav.classList.remove('nav-bar-active');
+    body.classList.remove('overflow-hidden');
+  });
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      mainNav.classList.remove('nav-bar-active');
+      body.classList.remove('overflow-hidden');
     });
-  }
+  });
 }
-mediaQuery.addListener(handleTabletChange);
-handleTabletChange(mediaQuery);
+
+mobileMenu();
